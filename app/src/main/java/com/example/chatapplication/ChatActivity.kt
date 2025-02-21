28bdae126461936968806d5fcc080c8b6df3233d
DESKTOP-KOS34VO\User <csh4757@naver.com>
2025-02-22 03:17:00 +0900

    [안드로이드 코틀린] 채팅앱 만드는 방법 part9 - 메시지 가져오기

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatapplication.databinding.ActivityChatBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ChatActivity : AppCompatActivity() {
 
@@ -25,11 +29,21 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var receiverRoom: String // 받는 대화방
     private lateinit var senderRoom: String // 보낸 대화방
 
+    private lateinit var messageList: ArrayList<Message>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        // 초기화
+        messageList = ArrayList()
+        val messageAdapter: MessageAdapter = MessageAdapter(this, messageList)
+
+        // RecyclerView
+        binding.chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.chatRecyclerView.adapter = messageAdapter
 
         // 넘어온 데이터 변수에 담기
         receiverName = intent.getStringExtra("name").toString()
@@ -61,8 +75,31 @@ class ChatActivity : AppCompatActivity() {
                     // 저장 성공하면
                     mDbRef.child("chats").child(receiverRoom).child("messages").push()
                         .setValue(messageObject)
-            }
+                }
+            // 입력 부분 초기화
+            binding.messageEdit.setText("")
         }
+
+        // 메시지 가져오기
+        mDbRef.child("chats").child(senderRoom).child("messages")
+            .addValueEventListener(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+
+                    for(postSnapShat in snapshot.children) {
+
+                        val message = postSnapShat.getValue(Message::class.java)
+                        messageList.add(message!!)
+                    }
+                    // 적용
+                    messageAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
