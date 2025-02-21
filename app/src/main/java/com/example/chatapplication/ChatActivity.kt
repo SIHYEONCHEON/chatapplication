a33ad9e1cb81fc7c985104d0996ce33e0be0280f
DESKTOP-KOS34VO\User <csh4757@naver.com>
2025-02-21 21:29:34 +0900

    [안드로이드 코틀린] 채팅앱 만드는 방법 part7 - 채팅 액티비티

---
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -0,0 +1,32 @@
+package com.example.chatapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ChatActivity : AppCompatActivity() {
+
+    private lateinit var receiverName: String
+    private lateinit var receiverUid: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chat)
+
+        // 넘어온 데이터 변수에 담기
+        receiverName = intent.getStringExtra("name").toString()
+        receiverUid = intent.getStringExtra("uId").toString()
+
+        // 액션바에 상대방 이름 보여주기
+        supportActionBar?.title = receiverName
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/chatapplication/UserAdapter.kt
+++ app/src/main/java/com/example/chatapplication/UserAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.chatapplication
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,10 +24,29 @@ RecyclerView.Adapter<UserAdapter.UserViewHolder>(){
      * 데이터 설정
      */
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+
+        // 데이터 담기
         val currentUser = userList[position]
+
+        // 화면에 데이터 보여주기
         holder.nameText.text = currentUser.name
+
+        // 아이템 클릭 이벤트
+        holder.itemView.setOnClickListener {
+
+            val intent = Intent(context, ChatActivity::class.java)
+
+            // 넘길 데이터
+            intent.putExtra("name", currentUser.name)
+            intent.putExtra("uId", currentUser.uId)
+
+            context.startActivity(intent)
+        }
     }
 
+    /**
+     * 데이터 갯수 가져오기
+     */
     override fun getItemCount(): Int {
         return userList.size
     }
